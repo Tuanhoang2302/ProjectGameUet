fba92f2a5d8e6a4dc9a7335b82207f8f5523429b
Tuanhoang2302 <55321074+Tuanhoang2302@users.noreply.github.com>
2019-10-30 19:00:00 +0700

    demo

--- Test/src/NormalEnemy.java
+++
@@ -1,5 +0,0 @@
-public class NormalEnemy extends Enemy {
-    public NormalEnemy(int id, int priceReward, int health, double speed, double armor, int dam, int point){
-        super(id, priceReward, health, speed, armor, dam, point);
-    }
-}
--- Test/src/SpawnPoint.java
+++
@@ -1,9 +0,0 @@
-public class SpawnPoint {
-    public int x;
-    public int y;
-
-    public SpawnPoint(int x, int y){
-        this.x = x;
-        this.y = y;
-    }
-}
